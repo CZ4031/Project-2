de1af6b272ea205a39bdacf3fd1a732f0c7a772a
roydonauyr <44868878+roydonauyr@users.noreply.github.com>
2022-10-23 23:24:12 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
 from preprocessing import validateConnect, executeQuery
 
 if __name__ == "__main__":
-    username = "postgres"
-    password = "Dragonslayer1999"
-    database_name = "TPC-H"
+    username = ""
+    password = ""
+    database_name = ""
     print("Welcome to CZ4003 database")
     # result = validateConnect(username, password, database_name)
     query = "SELECT * FROM customer"
