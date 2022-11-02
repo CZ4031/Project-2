ba854c61ee75652cd44d1193e948b8fc9f46f295
roydonauyr <roydonauyr@gmail.com>
2022-11-02 17:58:01 +0800

    Add logic for alternative query plans

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,87 @@ import psycopg2
 from decouple import config
 import json
 
+class PlanNode():
+
+    # Construct a node
+    def __init__(self) -> None:
+        self.parent = None
+        self.children = []
+        self.attributes = {}
+    
+    def print_tree(self):
+        queue = []
+        queue.append(self)
+        while len(queue) != 0:
+            childlength = len(queue)
+            # Add appropriate children ndoes
+            for i in range(childlength):
+                node = queue.pop(0)
+                if node != None:
+                    print(node.attributes['Node Type'] + "->cost: ", node.attributes['Total Cost'] , end='\n')
+                for child in node.children:
+                    queue.append(child)
+
+    def check_for_join(self):
+        queue = []
+        queue.append(self)
+        joins_used = {}
+        while len(queue)!=0:
+            childlength = len(queue)
+            for i in range(childlength):
+                node = queue.pop(0)
+                if "Join" in node.attributes['Node Type'] or "Loop" in node.attributes['Node Type']:
+                    joins_used[node.attributes['Node Type']] = int(node.attributes['Total Cost'])
+                for child in node.children:
+                    queue.append(child)
+        return joins_used
+
+
 class SetUp():
+    # Attributes
+    off_config = {
+        # Joins
+        "Hash Join" : "set enable_hashjoin=off",
+        "Merge Join" : "set enable_mergejoin=off",
+        "Nested Loop" : "set enable_nestloop=off",
+        # Scans
+        "Seq Scan" : "set enable_seqscan=off",
+        "Index Scan" : "set enable_indexscan=off",
+        "Bitmap Scan": "set enable_bitmapscan=off",
+        "Index Only Scan": "set enable_indexonlyscan=off",
+        "Tid Scan": "set enable_tidscan=off",
+        # Sort
+        "Sort" : "set enable_sort=off",
+        #Others
+        "Hash Agg": "set enable_hashagg=off",
+        "Material": "set enable_material=off",
+    }
+
+    on_config = {
+        # Joins
+        "Hash Join" : "set enable_hashjoin=on",
+        "Merge Join" : "set enable_mergejoin=on",
+        "Nested Loop" : "set enable_nestloop=on",
+        # Scans
+        "Seq Scan" : "set enable_seqscan=on",
+        "Index Scan" : "set enable_indexscan=on",
+        "Bitmap Scan": "set enable_bitmapscan=on",
+        "Index Only Scan": "set enable_indexonlyscan=on",
+        "Tid Scan": "set enable_tidscan=on",
+        # Sort
+        "Sort" : "set enable_sort=on",
+        #Others
+        "Hash Agg": "set enable_hashagg=on",
+        "Material": "set enable_material=on",
+    }
+
+    query_plans = {
+        "chosen_plan": "",
+        "alternative_plans": []
+    }
+
+
+
     # , host, port_num, database_name, username, password
     def __init__(self):
         # Setting up connection
@@ -22,25 +102,179 @@ class SetUp():
         except (Exception, psycopg2.DatabaseError):
             self.verify = False
 
-    def executeQuery(self, query):
+    def executeQuery(self, query, off=[]):
         optimalQEP = "EXPLAIN (ANALYSE, VERBOSE, COSTS, FORMAT JSON)" + query
 
         try:
+            # set cursor variable
             cursor = self.cursor
+            #Setting off for alternate query plans
+            for condition in off:
+                #print(self.off_config[condition])
+                # 5s timeout
+                cursor.execute("set statement_timeout = 5000")
+                cursor.execute(self.off_config[condition])
+            
             cursor.execute(optimalQEP)
+            
             explain_query = cursor.fetchall()
+
+            # Setting config back on to set up for next alternate query plan
+            for condition in off:
+                #print(self.on_config[condition])
+                cursor.execute(self.on_config[condition])
+            # write explain details into json file
+            with open('chosenQueryPlan.json', 'w') as output_file:
+                chosenQueryPlan = (json.dump(explain_query, output_file, ensure_ascii = False, indent = 4))
+
+            return explain_query[0][0][0]['Plan']
         except(Exception, psycopg2.DatabaseError) as error:
-            explain_query = "Please check your sql statement: \n" + text
-        finally: 
-            cursor.close()
+                
+                # Check how to seperate errors
+                print("Your error is: ", error)
+                # print("Your query is: ", query)
+                #explain_query = "Please check your sql statement: \n" + query
+                self.connection.rollback()
+                return "error"
+       
 
-        # write explain details into json file
-        with open('chosenQueryPlan.json', 'w') as output_file:
-            chosenQueryPlan = (json.dump(explain_query, output_file, ensure_ascii = False, indent = 4))
+    def add_attributes(self, plan, node):
+        """
+        Get current plan and insert the corresponding attributes into the current node 
+        """
+        for key,val in plan.items():
+            if key != "Plans":
+                node.attributes[key] = val
 
-        return chosenQueryPlan
+    def add_node(self, plan,node):
+        """
+        Recursive create nodes based on the number of "Plans" of current node (Each "Plans" corresponds to an additional child node)
+        """
+        # Break condition when no further nodes need to be created
+        if "Plans" not in plan:
+            return
+        for plan in plan["Plans"]:
+            # Create PlanNode
+            child = PlanNode()
+            child.parent = node
+            self.add_attributes(plan,child)
+            self.add_node(plan,child)
+            node.children.append(child)
 
-         # # Connect to Postgre database
+    def build_tree(self, plan):
+        """
+        Build the query plan tree
+        """
+        root = PlanNode()
+        self.add_attributes(plan, root)
+        self.add_node(plan,root)
+        return root
+
+   
+    def getAllQueryPlans(self, query):
+
+        #Original plan
+        plan = self.executeQuery(query)
+        root = self.build_tree(plan)
+        print("original join: ",root.check_for_join())
+        self.query_plans["chosen_plan"] = root
+        #connect.query_plans["chosen_plan"].print_tree()
+
+        #Alternate plans (Max: 11)
+        #Checking for AEP for Joins 
+        #Full Merge Join
+        plan = self.executeQuery(query, ["Nested Loop", "Hash Join"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("Merge join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        #Full hash join
+        plan = self.executeQuery(query, ['Nested Loop', "Merge Join"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("Hash join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        #Full nested loop join
+        plan = self.executeQuery(query, ['Merge Join', "Hash Join"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        #Checking for AEP for Scans
+        #Seq scan 
+        plan = self.executeQuery(query, ["Index Scan", "Bitmap Scan", "Index Only Scan", "Tid Scan"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        # Index Scan
+        plan = self.executeQuery(query, ["Seq Scan", "Bitmap Scan", "Index Only Scan", "Tid Scan"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        # Bitmap Scan
+        plan = self.executeQuery(query, ["Seq Scan", "Index Scan", "Index Only Scan", "Tid Scan"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+        
+        # Index Only Scan
+        plan = self.executeQuery(query, ["Seq Scan", "Index Scan", "Bitmap Scan", "Tid Scan"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        # Tid Only Scan
+        plan = self.executeQuery(query, ["Seq Scan", "Index Scan", "Bitmap Scan", "Index Only Scan"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        # Checking for AEP for Sort
+        # Sort
+        plan = self.executeQuery(query, ["Sort"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        # Checking for AEP for Others
+        # No Hash Agg
+        plan = self.executeQuery(query, ["Hash Agg"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        # No Material
+        plan = self.executeQuery(query, ["Material"])
+        if(plan != "error"):
+            alternate_root = self.build_tree(plan)
+            #print("NL join: ",root.check_for_join_type())
+            self.query_plans["alternative_plans"].append(alternate_root)
+
+        # Final print
+        print("length: ", len(self.query_plans['alternative_plans']))
+
+
+
+
+
+
+
+
+
+
+    # # Connect to Postgre database
     # def validateConnect(username, password, database_name):
     #     connection = None
     #     try:
--- project.py
+++ project.py
@@ -1,11 +1,40 @@
 from preprocessing import *
 
+
+
 if __name__ == "__main__":
     # username = "postgres"
     # password = ""
     # database_name = "TPC-H"
+
+
     print("Welcome to CZ4003 database")
+
+    # Setting up connection and queries
     connect = SetUp()
-    query = "SELECT * FROM customer"
-    result = connect.executeQuery(query)
-    print(result)
+    with open('query1.txt', 'r') as file:
+        data = file.read().replace('\n', ' ')
+    query = data
+    #query = "SELECT * FROM customer"
+    #query = "select * from customer C, orders O where C.c_custkey = O.o_custkey"
+     
+
+    #Executing queries
+    connect.getAllQueryPlans(query)
+
+    #Checking of tree
+    # connect.query_plans['chosen_plan'].print_tree()
+    # connect.query_plans['alternative_plans'][0].print_tree()
+
+    #Printing best plan tree
+    print("-------------------Best plan operator tree--------------")
+    print(connect.query_plans['chosen_plan'].print_tree())
+
+    #Printing AEP plan tree
+    print("-----------------Alternative plan operator tree----------------")
+    for plan in connect.query_plans['alternative_plans']:
+        print(plan.print_tree())
+        print("--------------------------------------------")
+
+    # close cursor
+    connect.cursor.close()
