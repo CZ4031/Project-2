2664fa1e5a8186b7a9bb3a432a69ccf89f7c4a42
Weiyi <ch3n.wei.yi@gmail.com>
2022-11-08 17:52:08 +0800

    Update annotation.py
    Updated necessary descriptions

--- annotation.py
+++ annotation.py
@@ -1,74 +1,98 @@
-from preprocessing import *
-from cgi import test
-from cgitb import text
-import psycopg2
-from decouple import config
-import json
-
-
 class Annotation:
-	def traverseTree(self):
-		return True
+	def traverseTree(self, root):
+		node = root
+		if not root:
+			return
+		while not node.children:
+			for child in node.children:
+				self.generateAnnotation(node)
+				node = child
+		return
 
 	def generateAnnotation(self, node):
 		for attribute in node.attributes:
-			# Start if else
-			attribute = attribute.lower()
+			attributeStr = attribute.lower()
 			annotation = ""
-			if attribute.contains("scan"):
-				if attribute.contains().contains("index"):
-					annotation = "Index scan used"
-				elif attribute.contains("sequential"):
-					annotation = "Sequential scan used"
+			# Add basic explanations for scans
+			if attributeStr.contains("scan"):
+				if attributeStr.contains("bitmap"):
+					if attributeStr.contains("index"):
+						annotation = "Bitmap index scan is used as multiple indices are constructed for this table.\n"
+					else:
+						annotation = "Bitmap heap scan is used as multiple indices as constructed. A heap is used to " \
+									 "sort the indices and quickly cut down the number of tuples scanned.\n "
+				elif attributeStr.contains("index"):
+					annotation = "Index scan is used to read the table as an index is constructed.\n"
+				elif attributeStr.contains("hash"):
+					annotation = "Hash scan is used to read the table as there are many unique values on the attribute " \
+								 "used to generate the index.\n"
 				else:
-					annotation = "Hash scan used"
+					annotation = "Sequential scan is used to read the table as there is no index.\n"
 				node.annotations.append(annotation)
 
-			## need to format all the below cases, for now is static
-			elif attribute.contains("Hash Join"):
+				# Add comparison
+				if not node.alternate_scan_dict:
+					for altScan in node.alternate_scan_dict:
+						annotation = f"Compared to {altScan}, {attribute} is {node.alternate_scan_dict.get(altScan)} " \
+									 f"times faster.\n"
+						node.annotations.append(annotation)
+
+			# need to format all the below cases, for now is static
+			# Add explanations for joins
+			elif attributeStr.contains("join"):
+				if attribute.contains("Hash"):
 					annotation = "Hash join is done on tables XX and YY on the conditions AA, to get resulting table BB.\n" 
+
+				elif attribute.contains("Merge"):
+					annotation = "Merge join is done on tables XX and YY on the conditions AA, to get resulting table BB.\n"
+
+				elif attribute.contains("Nested Loop"):
+					annotation = "Nested Loop join is done on tables XX and YY on the conditions AA, to get resulting table BB.\n"
+				node.annotations.append(annotation)
+
+				# Add comparison
+				if not node.alternate_scan_dict:
+					for altScan in node.alternate_scan_dict:
+						annotation = f"Compared to {altScan}, {attribute} is {node.alternate_scan_dict.get(altScan)} " \
+									 f"times faster.\n"
+						node.annotations.append(annotation)
+
+			# All other operators
+			else:
+				if attribute.contains("Hash"):
+					annotation = "Perform hashing on table.\n"
 					node.annotations.append(annotation)
 
-			elif attribute.contains("Hash"):
-					annotation = "Perform hashing on table XX, to get resulting table BB.\n" 
+				elif attribute.contains("Aggregate"):
+					annotation = "Perform aggregate on the table.\n"
 					node.annotations.append(annotation)
 
-			elif attribute.contains("Merge Join"):
-					annotation = "Merge join is done on tables XX and YY on the conditions AA, to get resulting table BB.\n" 
+				elif attribute.contains("Sort"):
+					annotation = "Sort table in an decremental manner.\n"
 					node.annotations.append(annotation)
 
-			elif attribute.contains("Nested Loop"):
-					annotation = "Nested Loop join is done on tables XX and YY on the conditions AA, to get resulting table BB.\n" 
+				elif attribute.contains("Incremental Sort"):
+					annotation = "Sort table in an incremental manner.\n"
 					node.annotations.append(annotation)
 
-			elif attribute.contains("Aggregate"):
-					annotation = "Perform aggregate on table XX, to get resulting table BB.\n" 
+				elif attribute.contains("Unique"):
+					annotation = "Duplicates are removed from the table.\n"
 					node.annotations.append(annotation)
 
-			elif attribute.contains("Sort"):
-					annotation = "Sort table XX in an decremental manner according to key YY, to get resulting table BB.\n" 
+				elif attribute.contains("Gather"):
+					annotation = "Gather operation is performed on the table.\n"
+
+				elif attribute.contains("Gather Merge"):
+					annotation = "Gather Merge operation is performed.\n"
 					node.annotations.append(annotation)
 
-			elif attribute.contains("Incremental Sort"):
-					annotation = "Sort table XX in an incremental manner according to key YY, to get resulting table BB.\n" 
-					node.annotations.append(annotation)
+				elif attribute.contains("Append"):
+					annotation = "Append tables.\n"
 
-			elif attribute.contains("Sort"):
-					annotation = "Sort table XX according to key YY, to get resulting table BB.\n" 
-					node.annotations.append(annotation)
+				elif attributeStr.contains("select"):
+					annotation = "Tuples fulfilling the conditions are selected.\n"
 
-			elif attribute.contains("Unique"):
-					annotation = "Duplicates are removed from table AA.\n" 
-					node.annotations.append(annotation)
+				elif attributeStr.contains("project"):
+					annotation = "Unnecessary elements are removed and the remaining elements are projected."
 
-			elif attribute.contains("Gather"):
-					annotation = "Gather operation is performed on table AA, to get resulting table BB.\n" 
-					node.annotations.append(annotation)
-
-			elif attribute.contains("Gather Merge"):
-					annotation = "Gather Merge operation is performed.\n" 
-					node.annotations.append(annotation)
-
-			elif attribute.contains("Append"):
-					annotation = "Append table XX to table YY, to get resulting table BB.\n" 
-					node.annotations.append(annotation)					
+				node.annotations.append(annotation)
